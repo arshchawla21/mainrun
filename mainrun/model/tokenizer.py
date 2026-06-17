0c7bca21e8d159cd263c5660fa86f1e512b801de
arshchawla21 <chawla.arsh@gmail.com>
2026-06-17 11:35:53 +0000

    Added Unigram and WordPiece tokenizers

--- mainrun/model/tokenizer.py
+++ mainrun/model/tokenizer.py
@@ -1,18 +1,43 @@
-from tokenizers import Tokenizer, models, trainers, pre_tokenizers, decoders
+from tokenizers import Tokenizer as HFTokenizer, models, trainers, pre_tokenizers, decoders
 
-def train_tokenizer(titles: list[str], vocab_size: int, unk_token: str = "<unk>", pad_token: str = "<pad>", eos_token: str = "<eos>") -> Tokenizer:
-    tokenizer = Tokenizer(models.BPE(unk_token=unk_token))
-    tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel()
-    tokenizer.decoder = decoders.ByteLevel()
-    trainer = trainers.BpeTrainer(
-        vocab_size=vocab_size,
-        special_tokens=[pad_token, eos_token, unk_token]
-    )
-    tokenizer.train_from_iterator(titles, trainer)
-    return tokenizer
 
-class BPETokenizer:
-    def __init__(self, tokenizer: Tokenizer):
+def train_tokenizer(token_type: str, titles: list[str], vocab_size: int,
+                    unk_token: str = "<unk>", pad_token: str = "<pad>",
+                    eos_token: str = "<eos>") -> HFTokenizer:
+    """Train a subword tokenizer of `token_type` on `titles`. All variants share a ByteLevel
+    pre-tokenizer + decoder so they operate on the same byte alphabet -> a fair vocab comparison."""
+    specials = [pad_token, eos_token, unk_token]
+
+    if token_type == "bpe":
+        tk = HFTokenizer(models.BPE(unk_token=unk_token))
+        tk.pre_tokenizer = pre_tokenizers.ByteLevel()
+        tk.decoder = decoders.ByteLevel()
+        trainer = trainers.BpeTrainer(vocab_size=vocab_size, special_tokens=specials)
+
+    # https://huggingface.co/docs/tokenizers/en/api/trainers#tokenizers.trainers.UnigramTrainer
+    elif token_type == "unigram":
+        tk = HFTokenizer(models.Unigram())                 # unk_id is set by the trainer below
+        tk.pre_tokenizer = pre_tokenizers.ByteLevel()
+        tk.decoder = decoders.ByteLevel()
+        trainer = trainers.UnigramTrainer(vocab_size=vocab_size, special_tokens=specials,
+                                          unk_token=unk_token)
+
+    # https://huggingface.co/docs/tokenizers/en/api/trainers#tokenizers.trainers.WordPieceTrainer
+    elif token_type == "wordpiece":
+        tk = HFTokenizer(models.WordPiece(unk_token=unk_token, max_input_chars_per_word=100))
+        tk.pre_tokenizer = pre_tokenizers.ByteLevel()
+        tk.decoder = decoders.ByteLevel()
+        trainer = trainers.WordPieceTrainer(vocab_size=vocab_size, special_tokens=specials)
+
+    else:
+        raise ValueError(f"unknown token_type {token_type!r} (expected: bpe | unigram | wordpiece)")
+
+    tk.train_from_iterator(titles, trainer)
+    return tk
+
+
+class Tokenizer:
+    def __init__(self, tokenizer: HFTokenizer):
         self.tk = tokenizer
         self.stoi = {tok: i for tok, i in tokenizer.get_vocab().items()}
         self.itos = {i: tok for tok, i in tokenizer.get_vocab().items()}
@@ -24,4 +49,5 @@ class BPETokenizer:
         return self.tk.decode(ids, skip_special_tokens=True)
 
     @property
-    def vocab_size(self): return self.tk.get_vocab_size()
+    def vocab_size(self):
+        return self.tk.get_vocab_size()
